17bf23921d1b672df20cb31b504daf2b736a071a
Mustago <muga200301@gmail.com>
2024-03-30 08:38:37 +0500

    initial commit add tailwind css

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <div class="my-3 text-center text-[30px] font-bold text-blue-900">
+      <h1>Todo List</h1>
+    </div>
+  </header>
+</template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+type TodosType = {
+  id?: number;
+  title?: string;
+  date?: string;
+};
+export default {
+  setup() {
+    const todos: TodosType[] = [];
+
+    return { todos };
+  },
+};
+</script>
+<template>
+  <div class="flex flex-col items-center">
+    <div class="w-4/5">
+      <form action="" class="flex flex-col items-center gap-4">
+        <input
+          class="w-full px-4 py-2 rounded-md border border-gray-800 shadow-sm text-base"
+          type="text"
+          placeholder="Введите дело"
+          required
+        />
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-[10px]"
+        >
+          Создать задач
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
